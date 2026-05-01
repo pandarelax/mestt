3b80e57293364854c0bbf8e8529149a292acbb58
Efehan Turhan <efehanturhan@gmail.com>
2026-05-02 02:10:40 +0300

    feat: add direnv and flake support

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "mestt development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.go
+              pkgs.ffmpeg
+            ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+              pkgs.wl-clipboard
+              pkgs.xclip
+            ];
+
+            shellHook = ''
+              export CGO_ENABLED=0
+            '';
+          };
+        }
+      );
+    };
+}
